ac2b642d22b659b32bd4c99d3ed58004ade91fdf
Zebulun Arendsee <arendsee@iastate.edu>
2017-08-27 13:53:58 -0500

    Possibly correct implementation of substitute

--- library/Benjamin.hs
+++ library/Benjamin.hs
@@ -2,6 +2,7 @@ module Benjamin (
     interpret
   , A1_Expr(..)
   , L1_Expr(..)
+  , L1DB_Expr(..)
   , step
   , walk
 ) where
--- library/Benjamin/Types.hs
+++ library/Benjamin/Types.hs
@@ -2,6 +2,7 @@ module Benjamin.Types
 (
     A1_Expr(..)
   , L1_Expr(..)
+  , L1DB_Expr(..)
   , step
   , walk
 ) where
@@ -53,6 +54,7 @@ instance Lang A1_Expr where
   is_value _           = False
 
 
+type ThrowsError = Either String
 
 type VarName = String
 
@@ -64,14 +66,41 @@ data L1_Expr
 -- Simply untyped lambda calculus with de Bruijn indices
 -- This format will be used internally
 data L1DB_Expr
-  = L1DB_Var Int
+  = L1DB_Var Integer
   | L1DB_App L1DB_Expr L1DB_Expr
-  | L1DB_Abs VarName L1DB_Expr
+  | L1DB_Abs VarName L1DB_Expr   -- VarName is needed to convert back to L1
+
+to_db_index :: L1_Expr -> L1DB_Expr
+to_db_index e = undefined
+
+-- Follows definition 6.2.1 from TPL
+shift :: L1DB_Expr -> L1DB_Expr
+shift e = undefined
+
+-- Adapted from definition 6.2.4 from TPL. 6.2.4 is used directly yields
+-- objects that mix indexes and symbols, which is ugly and evil. I fix this by
+-- using a list of index/symbol pairs, rather than just one. Also I add a new
+-- pair on every descent into an application.
+substitute :: L1DB_Expr -> [(Integer, String)] -> ThrowsError L1_Expr
+substitute (L1DB_Var k) js = case (lookup k js) of
+  Nothing -> Left "Aww shucks, couldn't find that number"
+  Just s  -> return $ L1_Var s
+substitute (L1DB_App a b) js = L1_App <$> (substitute a js) <*> (substitute b js)
+substitute (L1DB_Abs s a) js = L1_Abs s <$> (substitute a new_js) where
+  new_js = 
+    (map (\(i,x) -> (i+1,x)) js) -- Increment outer free variables by 1
+    ++
+    [(0, s)] -- Add the new locally bound variable
 
 instance Show L1_Expr where
   show (L1_Var x  ) = x
   show (L1_App a b) = "(" ++ (show a) ++ ")(" ++ (show b) ++ ")"
   show (L1_Abs x b) = "L " ++ x ++ " . " ++ (show b)
+
+instance Show L1DB_Expr where
+  show (L1DB_Var x  ) = show(x)
+  show (L1DB_App a b) = "(" ++ (show a) ++ ")(" ++ (show b) ++ ")"
+  show (L1DB_Abs _ b) = "L . " ++ (show b)
 
 instance Lang L1_Expr where
   step (L1_Var   x) = L1_Var x
