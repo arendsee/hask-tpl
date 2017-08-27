70a6e49033fff7401d86b6cedaec6b9e16d60454
Zebulun Arendsee <arendsee@iastate.edu>
2017-08-27 12:05:22 -0500

    Refactor naming conventions

--- library/Benjamin/Types.hs
+++ library/Benjamin/Types.hs
@@ -13,63 +13,70 @@ class Lang expr where
   walk :: expr -> [expr]
   walk e = case (is_value e) of
     True  -> [e]
-    False -> [e] ++ walk (step e) 
+    False -> [e] ++ walk (step e)
 
 
 
 data A1_Expr
-  = T 
-  | F 
-  | IfThen A1_Expr A1_Expr A1_Expr
-  | Zero
-  | Succ A1_Expr 
-  | Pred A1_Expr
-  | IsZero A1_Expr
-  | Stuck
+  = A1_T
+  | A1_F
+  | A1_IfThen A1_Expr A1_Expr A1_Expr
+  | A1_Zero
+  | A1_Succ A1_Expr
+  | A1_Pred A1_Expr
+  | A1_IsZero A1_Expr
+  | A1_Stuck
   deriving(Show)
 
 instance Lang A1_Expr where
-  step (IfThen T a _)     = a
-  step (IfThen F _ b)     = b
-  step (IfThen Zero  _ _) = Stuck
-  step (IfThen Stuck _ _) = Stuck
-  step (IfThen x a b)     = IfThen (step x) a b
-  step (Pred Zero)        = Zero
-  step (Pred (Succ x))    = x
-  step (IsZero Zero)      = T
-  step (IsZero (Succ _))  = F
-  step (IsZero T)         = Stuck
-  step (IsZero F)         = Stuck
-  step (IsZero x)         = IsZero (step x)
+  step (A1_IfThen A1_T a _)     = a
+  step (A1_IfThen A1_F _ b)     = b
+  step (A1_IfThen A1_Zero  _ _) = A1_Stuck
+  step (A1_IfThen A1_Stuck _ _) = A1_Stuck
+  step (A1_IfThen x a b)        = A1_IfThen (step x) a b
+  step (A1_Pred A1_Zero)        = A1_Zero
+  step (A1_Pred (A1_Succ x))    = x
+  step (A1_IsZero A1_Zero)      = A1_T
+  step (A1_IsZero (A1_Succ _))  = A1_F
+  step (A1_IsZero A1_T)         = A1_Stuck
+  step (A1_IsZero A1_F)         = A1_Stuck
+  step (A1_IsZero x)            = A1_IsZero (step x)
   step x = case (is_value x) of
     True  -> x
-    False -> Stuck
+    False -> A1_Stuck
 
-  is_value T        = True
-  is_value F        = True
-  is_value Zero     = True
-  is_value (Succ _) = True
-  is_value Stuck    = True
-  is_value _        = False
+  is_value A1_T        = True
+  is_value A1_F        = True
+  is_value A1_Zero     = True
+  is_value (A1_Succ _) = True
+  is_value A1_Stuck    = True
+  is_value _           = False
 
 
 
 type VarName = String
 
 data L1_Expr
-  = Variable VarName
-  | Application L1_Expr L1_Expr
-  | Abstraction VarName L1_Expr  
+  = L1_Var VarName
+  | L1_App L1_Expr L1_Expr
+  | L1_Abs VarName L1_Expr
+
+-- Simply untyped lambda calculus with de Bruijn indices
+-- This format will be used internally
+data L1DB_Expr
+  = L1DB_Var Int
+  | L1DB_App L1DB_Expr L1DB_Expr
+  | L1DB_Abs VarName L1DB_Expr
 
 instance Show L1_Expr where
-  show (Variable x) = x
-  show (Application a b) = "(" ++ (show a) ++ ")(" ++ (show b) ++ ")"
-  show (Abstraction x b) = "L " ++ x ++ " . " ++ (show b)
+  show (L1_Var x  ) = x
+  show (L1_App a b) = "(" ++ (show a) ++ ")(" ++ (show b) ++ ")"
+  show (L1_Abs x b) = "L " ++ x ++ " . " ++ (show b)
 
 instance Lang L1_Expr where
-  step (Variable x) = Variable x
-  step (Application _ _) = undefined
-  step (Abstraction _ _) = undefined
+  step (L1_Var   x) = L1_Var x
+  step (L1_App _ _) = undefined
+  step (L1_Abs _ _) = undefined
 
-  is_value (Variable _) = True
+  is_value (L1_Var _) = True
   is_value _ = False
